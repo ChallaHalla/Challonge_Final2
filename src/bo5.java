60e4e0edb8089eb824dc51d0a154bb184c701a0f
ChallaHalla <sc5455@nyu.edu>
2017-11-11 15:39:20 -0500

    Update bo5.java

--- src/bo5.java
+++ src/bo5.java
@@ -1,17 +1,28 @@
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class bo5 extends Set implements Serializable{
 	private Game[] games = new Game[5];
+	private ArrayList<player> gameWins = new ArrayList<player>();
+	player temp;
 	
 	
 	public void addGame(int game_num, int stage, player p1, player p2){
+		
 		this.games[game_num].setWinner(p1);
 		this.games[game_num].setLoser(p2);
 		this.games[game_num].setStage(stage);
+		if(game_num > 1){
+			if(Collections.frequency(this.gameWins, p1) == 3){
+				this.setWinner(p1);
+				this.setLoser(p2);
+			}
+		}
 	}
 	bo5(){
 		for(int i=0; i<5; i++){
-			this.games[i] = new Game();
+			this.games[i] =  new Game(i,-1, temp, temp);
 		}
 		
 	}
@@ -23,5 +34,10 @@ public class bo5 extends Set implements Serializable{
 	public Game getGame(int game_num){
 		return this.games[game_num];
 	}
-
+	
+	public void getStages(ArrayList<Integer> bannedStages) {
+		for(int i=0; i<this.games.length; i++){
+			bannedStages.add(this.games[i].getStage());
+		}
+	}
 }
